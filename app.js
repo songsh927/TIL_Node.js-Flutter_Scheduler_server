0615f93657807d6685a4891634593b88a46153df
songseunghyun <songsh96927@gmail.com>
2022-01-26 14:02:24 +0900

    2022.01.26

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import 'express-async-errors';
+import scheduleRouter from './router/schedule.js';
+import memoRouter from './router/memo.js';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors('*'));
+app.use(morgan('tiny'));
+
+app.set('etag', false);
+
+app.use('/scheduler' , scheduleRouter);
+app.use('/memo' , memoRouter);
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.sendStatus(500);
+})
+
+
+app.listen(3000);
---
+++ router/schedule.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import 'express-async-errors';
+
+const router = express.Router();
+
+let data = [
+    {
+        "id": 1,
+        "date": "2022,01,17",
+        "title": "운동",
+        "text": "6시 운동",
+      },
+      {
+        "id": 2,
+        "date": "2022,01,17",
+        "title": "술약속",
+        "text": "8시 약속",
+      },
+      {
+        "id": 3,
+        "date": "2022,01,18",
+        "title": "운동",
+        "text": "오전 10시 운동",
+      },
+      {
+        "id": 4,
+        "date": "2022,01,19",
+        "title": "도수치료",
+        "text": "오후 4시",
+      },
+]
+
+router.get('/' , (req, res, next) => {
+    res.status(200).json(data);
+});
+
+
+export default router;
